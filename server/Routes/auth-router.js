c6cb1fbb2fe31b84f63147887e516737ca8157a1
shubham <shubhamgogawale1@gmail.com>
2024-08-13 11:42:21 +0530

    Verify JWT token

--- server/Routes/auth-router.js
+++ server/Routes/auth-router.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller.js");
 const signupSchema = require("../validators/auth-validator.js");
 const validate = require("../middlewares/validate-middleware.js");
+const authMiddleware = require("../middlewares/auth-middleware.js")
 
 router.route("/").get(authcontrollers.home);
+
 router
   .route("/register")
   .post(validate(signupSchema), authcontrollers.register);
+
 router.route("/login").post(authcontrollers.login);
+
+router.route("user").get(authcontrollers.user)
 
 module.exports = router;
